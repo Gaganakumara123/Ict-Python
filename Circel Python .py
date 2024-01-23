d3a479d69ac781a34d46bdcaadf745dde63d1711
Manupa Gagana <155724666+manupagagana@users.noreply.github.com>
2024-01-23 20:30:09 +0530

    Update Python Codes
    This Is A Python Code

---
+++ Circel Python .py
@@ -0,0 +1,8 @@
+import math;
+
+x = int(input("Enter Circel Redius :"))
+
+
+y = math.pi * x * x
+
+print(y)
---
+++ ODD Even Numbers .py
@@ -0,0 +1,20 @@
+
+
+x = int(input("Please Enter you Number :"))
+
+
+if (x <=100):
+
+    if (x % 2 == 0):
+
+        print("This is a Even Number")
+
+    else:
+
+        print("This is a Odd Number")
+else:
+
+    print("Please Enter Renumber")
+
+
+
